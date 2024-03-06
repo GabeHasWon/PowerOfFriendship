906de682b1a08fbf180774e48ab1cf7c74ff84de
GabeHasWon <gabriel@guevarameyer.com>
2024-03-05 19:51:01 -0600

    recipes

---
+++ Content/Globals/ItemGlobals/RecipeGlobal.cs
@@ -0,0 +1,27 @@
+﻿namespace PoF.Content.Globals.ItemGlobals;
+
+internal class RecipeGlobal : GlobalItem
+{
+    public override void AddRecipes()
+    {
+        // Wand of Sparking recipe
+        Recipe.Create(ItemID.WandofSparking)
+            .AddIngredient(ItemID.Wood, 5)
+            .AddIngredient(ItemID.Torch, 2)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+
+        // Leather from Vertebrae recipe
+        Recipe.Create(ItemID.Leather)
+            .AddIngredient(ItemID.Vertebrae, 5)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+
+        // Leather Whip recipe
+        Recipe.Create(ItemID.BlandWhip)
+            .AddIngredient(ItemID.Leather, 8)
+            .AddIngredient(ItemID.Rope, 10)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+    }
+}
